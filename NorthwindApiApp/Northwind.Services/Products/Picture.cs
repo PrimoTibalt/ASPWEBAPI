c4218f2dd544e4030d604d6ed068c8f601597f3a
= <anton.shheglov.1@gmail.com>
2020-03-28 13:21:53 +0300

    add: model Picture for ProductCategory.

---
+++ NorthwindApiApp/Northwind.Services/Products/Picture.cs
@@ -0,0 +1,47 @@
+﻿namespace Northwind.Services.Products
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// Represent picture for each category.
+    /// </summary>
+    public class Picture
+    {
+        /// <summary>
+        /// Gets or sets id of picture.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets picture content(the image).
+        /// </summary>
+        public byte[] Content { get; set; }
+
+        /// <summary>
+        /// Gets or sets picture name in filesystem.
+        /// </summary>
+        [Display(Name = "File Name")]
+        public string UntrustedName { get; set; }
+
+        /// <summary>
+        /// Gets or sets a note for a picture.
+        /// </summary>
+        [Display(Name = "Note")]
+        public string Note { get; set; }
+
+        /// <summary>
+        /// Gets or sets size of a picture.
+        /// </summary>
+        [Display(Name = "Size (bytes)")]
+        [DisplayFormat(DataFormatString = "{0:N0}")]
+        public long Size { get; set; }
+
+        /// <summary>
+        /// Gets or sets time, when picture was add.
+        /// </summary>
+        [Display(Name = "Uploaded (UTC)")]
+        [DisplayFormat(DataFormatString = "{0:G}")]
+        public DateTime UploadDT { get; set; }
+    }
+}
