74ea431c7956dacef673051c97c5c2b32bd6d871
= <anton.shheglov.1@gmail.com>
2020-03-28 18:16:48 +0300

    add: ProductCategoryPictureService and ProductCategoryManagementService classes.

---
+++ NorthwindApiApp/Northwind.Services/Products/ProductCategoryManagementService.cs
@@ -0,0 +1,130 @@
+﻿namespace Northwind.Services.Products
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using Northwind.Services.Context;
+
+    /// <summary>
+    /// Represents a stub for a product categories management service.
+    /// </summary>
+    public class ProductCategoryManagementService : IProductCategoryManagementService
+    {
+        private NorthwindContext Context { get; set; } = new NorthwindContext(new Microsoft.EntityFrameworkCore.DbContextOptions<NorthwindContext>());
+
+        /// <summary>
+        /// Transmits string to ProductCategory instance.
+        /// </summary>
+        /// <param name="from">String in format "ID:value,Name:value,Description:value".</param>
+        /// <returns>Instance, gotten from string.</returns>
+        public static ProductCategory FromStrToProductCategory(string from)
+        {
+            if (string.IsNullOrWhiteSpace(from))
+            {
+                throw new ArgumentNullException(nameof(from));
+            }
+
+            ProductCategory to = new ProductCategory();
+            var splited = from.Split(',');
+            to.Id = int.Parse(splited[0].Split(':')[1], CultureInfo.InvariantCulture);
+            to.Name = splited[1].Split(':')[1];
+            to.Description = splited[2].Split(':')[1];
+            return to;
+        }
+
+        /// <inheritdoc/>
+        public int CreateCategory(ProductCategory productCategory)
+        {
+            if (productCategory is null)
+            {
+                throw new ArgumentNullException(nameof(productCategory));
+            }
+
+            this.Context.CreateNewCategory(productCategory);
+            return productCategory.Id;
+        }
+
+        /// <inheritdoc/>
+        public bool DestroyCategory(int categoryId)
+        {
+            try
+            {
+                this.Context.DeleteCategory(categoryId);
+                return true;
+            }
+            catch (KeyNotFoundException)
+            {
+                return false;
+            }
+        }
+
+        /// <inheritdoc/>
+        public IList<ProductCategory> LookupCategoriesByName(IList<string> names)
+        {
+            if (names is null)
+            {
+                throw new ArgumentNullException(nameof(names));
+            }
+
+            List<ProductCategory> categoriesWithSameName = new List<ProductCategory>();
+            foreach (var category in this.Context.GetCategories())
+            {
+                ProductCategory currentCategory = ProductCategoryManagementService.FromStrToProductCategory(category);
+                if (names.Contains(currentCategory.Name))
+                {
+                    categoriesWithSameName.Add(currentCategory);
+                }
+            }
+
+            return categoriesWithSameName;
+        }
+
+        /// <inheritdoc/>
+        public IList<ProductCategory> ShowCategories(int offset, int limit)
+        {
+            List<ProductCategory> categories = new List<ProductCategory>();
+            foreach (var category in this.Context.GetCategories())
+            {
+                categories.Add(ProductCategoryManagementService.FromStrToProductCategory(category));
+            }
+
+            return categories;
+        }
+
+        /// <inheritdoc/>
+        public bool TryShowCategory(int categoryId, out ProductCategory productCategory)
+        {
+            foreach (var category in this.Context.GetCategories())
+            {
+                productCategory = ProductCategoryManagementService.FromStrToProductCategory(category);
+                if (productCategory.Id == categoryId)
+                {
+                    return true;
+                }
+            }
+
+            productCategory = new ProductCategory();
+            return false;
+        }
+
+        /// <inheritdoc/>
+        public bool UpdateCategories(int categoryId, ProductCategory productCategory)
+        {
+            try
+            {
+                if (this.Context.UpdateCategory(productCategory))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (ArgumentNullException)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ NorthwindApiApp/Northwind.Services/Products/ProductCategoryPictureService.cs
@@ -0,0 +1,86 @@
+﻿namespace Northwind.Services.Products
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+    using Northwind.Services.Context;
+
+    /// <summary>
+    /// Represents a stub for a product categories picture management service.
+    /// </summary>
+    public class ProductCategoryPictureService : IProductCategoryPicturesService
+    {
+        private NorthwindContext Context { get; set; } = new NorthwindContext(new Microsoft.EntityFrameworkCore.DbContextOptions<NorthwindContext>());
+
+        /// <inheritdoc/>
+        public bool DestroyPicture(int categoryId)
+        {
+            try
+            {
+                var category = new ProductCategory();
+                if (new ProductCategoryManagementService().TryShowCategory(categoryId, out category))
+                {
+                    File.Delete("C:" + category.Description);
+                    category.Description = "No Picture";
+                    new ProductCategoryManagementService().UpdateCategories(categoryId, category);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (KeyNotFoundException)
+            {
+                return false;
+            }
+            catch (DirectoryNotFoundException)
+            {
+                return false;
+            }
+        }
+
+        /// <inheritdoc/>
+        public bool TryShowPicture(int categoryId, out byte[] bytes)
+        {
+            foreach (var category in this.Context.GetCategories())
+            {
+                var categoryObj = ProductCategoryManagementService.FromStrToProductCategory(category);
+                if (categoryObj.Id == categoryId)
+                {
+                    try
+                    {
+                        bytes = File.ReadAllBytes(categoryObj.Description);
+                        return true;
+                    }
+                    catch (FileNotFoundException)
+                    {
+                        bytes = null;
+                        return false;
+                    }
+                }
+            }
+
+            bytes = null;
+            return false;
+        }
+
+        /// <inheritdoc/>
+        public bool UpdatePicture(int categoryId, Stream stream)
+        {
+            foreach (var categoryRow in this.Context.GetCategories())
+            {
+                var categoryObj = ProductCategoryManagementService.FromStrToProductCategory(categoryRow);
+                if (categoryId == categoryObj.Id)
+                {
+                    categoryObj.Description = $"\\Users\\PrimoTibalt\\Desktop\\Studying\\C#\\ASPWEBAPI\\NorthwindApiApp\\NorthwindApiApp\\obj\\images\\{categoryObj.Id}";
+                    new ProductCategoryManagementService().UpdateCategories(categoryId, categoryObj);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
