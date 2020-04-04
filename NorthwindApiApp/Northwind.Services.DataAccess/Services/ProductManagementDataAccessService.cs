67c86219b2679234d2cd2cc2c041ce6e8bf66bbd
= <anton.shheglov.1@gmail.com>
2020-04-04 08:41:17 +0300

    add: Initial Northwind.Services.DataAccess.

---
+++ NorthwindApiApp/Northwind.Services.DataAccess/Services/ProductManagementDataAccessService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Northwind.Services.Products;
+
+namespace Northwind.Services.DataAccess.Services
+{
+    public class ProductManagementDataAccessService : IProductManagementService
+    {
+        public int CreateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DestroyProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Product> LookupProductsByName(IList<string> names)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Product> ShowProducts(int offset, int limit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Product> ShowProductsForCategory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryShowProduct(int productId, out Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateProduct(int productId, Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
